b2a585e78fe03b3247dc4c5e2e5d153777877e77
mimilili <dawnlin15@gmail.com>
2020-12-02 17:07:10 -0500

    quiz app with js and jquery

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /**
  * Example store structure
  */
+
+ /*
 const store = {
   // 5 or more questions are required
   questions: [
@@ -29,6 +31,7 @@ const store = {
   questionNumber: 0,
   score: 0
 };
+*/
 
 /**
  * 
@@ -56,3 +59,207 @@ const store = {
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+const store = {
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'Who painted the <i>Mona Lisa</i>?',
+      answers: [
+        'Leonardo Da Vinci',
+        'Raphael',
+        'Leonardo DiCaprio',
+        'Michelangelo'
+      ],
+      correctAnswer: 'Leonardo Da Vinci'
+    },
+    {
+      question: 'Which art painting of Van Gogh became the most famous piece?',
+      answers: [
+        '<i>Cafe Terrace At Night</i>',
+        '<i>Sunflowers</i>',
+        '<i>The Starry Night</i>',
+        '<i>Almond Blossom</i>'
+      ],
+      correctAnswer: '<i>The Starry Night</i>'
+    },
+    {
+      question: 'The artist Kandinsky is considered the first for which type of art?',
+      answers: [
+        'Fauvism',
+        'Cubism',
+        'Abstract',
+        'Avant-garde'
+      ],
+      correctAnswer: 'Abstract'
+    },
+    {
+      question: 'Which Renaissance artist is the most famous procrastinator of all time?',
+      answers: [
+        'Salvador Dalí',
+        'Vincent van Gogh',
+        'Leonardo Da Vinci',
+        'Donatello'
+      ],
+      correctAnswer: 'Leonardo Da Vinci'
+    },
+    {
+      question: 'Which artist is famous for Pop Art?',
+      answers: [
+        'Helen Frankenthaler',
+        'Andy Warhol',
+        'Cy Twombly',
+        'Frank Stella'
+      ],
+      correctAnswer: 'Andy Warhol'
+    },
+    {
+      question: 'What are the three primary colors?',
+      answers: [
+        'Red,Green,Blue',
+        'Purple,Green,Orange',
+        'Red,Orange,Blue',
+        'Red,Yellow,Blue'
+      ],
+      correctAnswer: 'Red,Yellow,Blue'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
+
+let currentQuestion = 0;
+let score = 0;
+
+function renderStartPage() {
+  /* the first page of the quiz */
+return (`
+  <h1>
+    Art Quiz
+  </h1>
+  <section>
+  <p> 
+    Let's find out how much you know about ART!
+  </p>
+  <button type="submit" class="start-button">
+    Start Quiz
+  </button>
+  </section>
+`);
+}
+/**
+ * <h3>question that's on</h3>
+ * <div>answer choices</div>
+ */
+function renderQuestionPage(){
+  return(`
+  <h1>
+  Art Quiz
+  </h1>
+  <section>
+  <h4>
+  Question:${currentQuestion+1}/6
+  </h4>
+  <h4>
+  Score:${score}/6
+  </h4>
+  <h3>
+  ${store.questions[currentQuestion].question}
+  </h3>
+  <div>
+    <input type="radio" name="choice" value="${store.questions[currentQuestion].answers[0]}"/>
+     ${store.questions[currentQuestion].answers[0]}
+  </div>
+  <div>
+    <input type="radio" name="choice" value="${store.questions[currentQuestion].answers[1]}"/>
+    ${store.questions[currentQuestion].answers[1]}
+  </div>
+  <div>
+    <input type="radio" name="choice" value="${store.questions[currentQuestion].answers[2]}"/>
+     ${store.questions[currentQuestion].answers[2]}
+  </div>
+  <div>
+    <input type="radio" name="choice" value="${store.questions[currentQuestion].answers[3]}"/>
+     ${store.questions[currentQuestion].answers[3]}
+  </div>
+  <button type="submit" class="submit-button">
+  Submit
+</button>
+</section>
+ ` );
+}
+ 
+function renderFeedBackPageCorrect(){
+  return (`
+  <h2>
+  That's Correct!
+  </h2>
+  <button type="submit" class="next-button">
+ Next
+</button>
+  `)
+}
+
+function renderFeedBackPageWrong(){
+  return (`
+  <h2>
+  whoops, the correct answer is ${store.questions[currentQuestion].correctAnswer}!
+  </h2>
+  <button type="submit" class="next-button">
+ Next
+</button>
+  `)
+}
+
+/**
+ * startQuiz sending it to the first question page
+ */
+function startQuizHandler(){
+$(".quiz-app").on("click", ".start-button", function(event){
+  event.preventDefault();
+  $(".quiz-app").html( renderQuestionPage );
+  store.quizStarted = true;
+});
+}
+
+function submitHandler(){
+$(".quiz-app").on("click", ".submit-button", function(event){
+  event.preventDefault();
+  let selectedAnswer = $('input:checked').val();
+  let answer = store.questions[currentQuestion].correctAnswer;
+
+  if(selectedAnswer === answer){
+    score++;
+    $(".quiz-app").html( renderFeedBackPageCorrect);
+  }
+  else{
+    if(selectedAnswer === undefined){
+      alert("Please select an answer!");
+    }else{
+      $(".quiz-app").html( renderFeedBackPageWrong);
+    }
+  }
+  
+});
+}
+
+function nextHandler(){
+  $(".quiz-app").on("click", ".next-button", function(event){
+    event.preventDefault();
+    currentQuestion++;
+    $(".quiz-app").html( renderQuestionPage );
+  });
+}
+
+function handleQuiz() {
+  $(".quiz-app").html( renderStartPage );
+
+  $(submitHandler);
+  $(startQuizHandler);
+  $(nextHandler);
+
+  
+}
+
+$(handleQuiz);
